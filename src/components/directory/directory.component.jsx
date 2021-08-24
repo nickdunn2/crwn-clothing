7c0fd8053f0a1383536541385bb90b0fddf820d1
Nick Dunn <nick.dunn@flosports.tv>
2021-08-24 09:58:46 -0500

    style: add dynamic styles to menu-items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,10 +48,12 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id}) => (
+          this.state.sections.map(({ title, imageUrl, id, size}) => (
             <MenuItem
               key={ id }
               title={ title }
+              imageUrl={ imageUrl }
+              size={ size }
             />
           ))
         }
