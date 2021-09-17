56d6ab098fad8f059e6404d46e46269d5c16bec7
Nick Dunn <nick.dunn@flosports.tv>
2021-09-17 09:45:32 -0500

    feat: introduce base shop page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import './App.css'
-import HomePage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import './App.css'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   )
