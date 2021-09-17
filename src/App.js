8be7948b5908994913c2842e8702396eebcb1ba0
Nick Dunn <nick.dunn@flosports.tv>
2021-09-17 09:39:43 -0500

    refactor: make homepage its own directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import HomePage from './pages/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
 
 const HatsPage = () => (
