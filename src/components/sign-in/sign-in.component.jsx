c8bf83abcfdb5b0c8d25c29c82fbd26547bebb82
Nick Dunn <nick.dunn@flosports.tv>
2021-10-27 09:29:53 -0500

    style: improve styling on sign-in component/buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,12 +47,14 @@ class SignIn extends React.Component {
             label="password"
             handleChange={ this.handleChange }
             required />
-          <CustomButton type="submit" value="Submit Form">
-            Sign In
-          </CustomButton>
-          <CustomButton onClick={ signInWithGoogle }>
-            Sign In With Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit Form">
+              Sign In
+            </CustomButton>
+            <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
