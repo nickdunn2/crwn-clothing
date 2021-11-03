6c6b4e99e7ef510796db6bf85f5944b63ac2981e
Nick Dunn <nick.dunn@flosports.tv>
2021-11-03 09:54:04 -0500

    feat(auth): store new users in firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   appId: "1:646825184777:web:0b4d44601c6a7d9cbbb2d8"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapshot = await userRef.get()
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (e) {
+      console.error('error creating user', e.message)
+    }
+  }
+
+  return userRef
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
