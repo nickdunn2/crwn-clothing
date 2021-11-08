bd5654b106f32dd0c642c78576373a9ab1fb4667
Nick Dunn <nick.dunn@flosports.tv>
2021-11-08 09:48:55 -0600

    feat(auth): introduce sign-up component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,10 @@ class SignIn extends React.Component {
         <h2 className="title">I already have an account.</h2>
         <span>Sign in with your email and password.</span>
 
-        <form onSubmit={ this.handleSubmit }>
+        <form
+          className="sign-in-form"
+          onSubmit={ this.handleSubmit }
+        >
           <FormInput
             name="email"
             type="email"
