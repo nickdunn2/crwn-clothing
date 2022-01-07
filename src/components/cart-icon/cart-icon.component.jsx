6e9dd6aff94258d8648f896c90c6b781b0beabaf
Nick Dunn <nick.dunn@flosports.tv>
2022-01-07 09:31:23 -0600

    feat: add (non-memoized) itemCount to cart-icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,19 @@ import './cart-icon.styles.scss'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { connect } from 'react-redux'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={ toggleCartHidden }>
     <ShoppingIcon className="shopping-icon"/>
-    <span className="item-count">0</span>
+    <span className="item-count">{ itemCount }</span>
   </div>
 )
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
