5eddaf1840635ee4c2b8ecaf9f923a43be1e3fbb
Nick Dunn <nick.dunn@flosports.tv>
2022-01-26 10:11:22 -0600

    feat: introduce user selectors and createStructuredSelector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import './cart-icon.styles.scss'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { connect } from 'react-redux'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
+import { createStructuredSelector } from 'reselect'
 
 const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={ toggleCartHidden }>
@@ -12,8 +13,8 @@ const CartIcon = ({ itemCount, toggleCartHidden }) => (
   </div>
 )
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 })
 
 const mapDispatchToProps = dispatch => ({
