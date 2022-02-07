bef5045b68b6c759175df1c0ae11f51f2e03d50d
Nick Dunn <nick.dunn@flosports.tv>
2022-02-07 09:43:24 -0600

    feat: add checkout page basics

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,8 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
 )
+
+export const selectCartTotel = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((acc, cartItem) => acc + (cartItem.price * cartItem.quantity), 0)
+)
